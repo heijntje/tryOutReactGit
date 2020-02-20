d1cbc82c5db9461bfbea3537e30b602199596029
Niek Heijnen <n.heijnen@student.science.ru.nl>
2020-02-20 17:41:09 +0100

    finished tutorial

--- reacttodo/src/components/Todos.js
+++ reacttodo/src/components/Todos.js
@@ -5,16 +5,17 @@ import PropTypes from 'prop-types';
 class Todos extends Component {
   render() {
     return this.props.todos.map((todo) => (
-      <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
+        <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
     ));
   }
 }
 
 // PropTypes
 Todos.propTypes = {
-  todos: PropTypes.array.isRequired,
-  markComplete: PropTypes.func.isRequired,
-  delTodo: PropTypes.func.isRequired,
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
+
 }
 
 export default Todos;
